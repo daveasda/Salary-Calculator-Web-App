2c22b4f670a95e612208000966b8cbc1757f0b5f
Devni Dharmasooriya <153256670+daveasda@users.noreply.github.com>
2026-09-24 17:54:37 +0530

    Update CORS for deployed frontend

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,10 @@ import employee from "./routes/employee.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: "https://salary-calculator-web-app-frontend.onrender.com"
+}));
+
 const port = process.env.PORT || 5000;
 
 app.use(express.json());

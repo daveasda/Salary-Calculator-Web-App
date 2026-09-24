2621959f1164ed21faddcbd6739443f68f55781c
Devni Dharmasooriya <153256670+daveasda@users.noreply.github.com>
2026-09-24 17:36:51 +0530

    Fix CORS

--- backend/server.js
+++ backend/server.js
@@ -7,10 +7,7 @@ import employee from "./routes/employee.js";
 dotenv.config();
 
 const app = express();
-app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-  credentials: true
-}));
+app.use(cors());
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
